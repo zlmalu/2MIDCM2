cb01bd3c91945a8d8ef4e8081ed8105e6a7af4cd
zlmalu <1992123836@qq.com>
2021-04-26 17:00:19 +0800

    1.1
    common.php 257sssssssssssssssss

--- data/config/mysql_config.php
+++ data/config/mysql_config.php
@@ -1,8 +1,8 @@
 <?php 
 if(!defined('BASEPATH'))exit('No direct script access allowed');
 define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'erp');       //数据账号
-define('DB_PWD', 'yuanli');            //密码
-define('DB_NAME', 'erpTest');    //数据库名称
+define('DB_USER', 'root');       //数据账号
+define('DB_PWD', 'root');            //密码
+define('DB_NAME', 'erp');    //数据库名称
 /* End of file constant.php */
 /* Location: ./shared/config/constant.php */
